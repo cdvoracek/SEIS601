efbd087f8622a7e70e7d548538eeae3998b1a162
cdvoracek <chad.dvoracek@gmail.com>
2013-02-23 15:21:29 -0600

    commitone

---
+++ BlueJ-SEIS601/Dvoracek_Chad_Asgn_2/App.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class App here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class App
+{
+ 
+public static void main(String[] args) 
+{
+
+NumberFun numFun = new NumberFun();
+//Part A
+//numFun.isNumberOdd(0)
+
+}
+}
