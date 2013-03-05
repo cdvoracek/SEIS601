ac81f750277466f3b17f495f9b59c91e4b4d4ff8
cdvoracek <chad.dvoracek@gmail.com>
2013-03-04 23:48:13 -0600

    Com 2

--- BlueJ-SEIS601/Dvoracek_Chad_Asgn_2/NumberFun.java
+++ BlueJ-SEIS601/Dvoracek_Chad_Asgn_2/NumberFun.java
@@ -5,19 +5,20 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+
 public class NumberFun
 {
 
+    boolean isNumberOdd(int num)
+   {num=8;
+    
+    if (num/2 = type.int){ 
+        isNumberOdd=false;	
+    
+    else {isNumberOdd=true;
+    }
+    System.out.println (isNumberOdd);  
 
-   boolean isNumberOdd(int num);
-
-num=8
-
-if (num/2 = type int)
-    isNumberOdd=false;
-    else isNumberOdd=true;
- 
-System.out.println (isNumberOdd)  
-   
+}
 }
 
