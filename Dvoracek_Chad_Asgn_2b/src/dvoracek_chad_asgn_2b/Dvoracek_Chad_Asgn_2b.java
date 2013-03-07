e76f076c1d2027d22a4a63ff50b93c26dc838365
cdvoracek <chad.dvoracek@gmail.com>
2013-03-06 22:55:09 -0600

    com5
    Final possible

--- Dvoracek_Chad_Asgn_2b/src/dvoracek_chad_asgn_2b/Dvoracek_Chad_Asgn_2b.java
+++ Dvoracek_Chad_Asgn_2b/src/dvoracek_chad_asgn_2b/Dvoracek_Chad_Asgn_2b.java
@@ -17,19 +17,22 @@ public class Dvoracek_Chad_Asgn_2b {
 
     //declare variables
         
-    int castnum;
+    int castnum,
+        castnum2,
+        random1,
+        random2;
     
    
        
-    /** Deliverable A
+    /** Deliverables A
      *  Provide a value for the method NumberFun and test
-     *  if the number is odd or even.
+     *  if the number is odd.
      */
     
         for (castnum=0; castnum<2; castnum++)
-        { NumberFun numFun = new NumberFun();
-  
-        boolean numberChoice = numFun.isNumberOdd(castnum);
+        { 
+            NumberFun numFun = new NumberFun();
+            boolean numberChoice = numFun.isNumberOdd(castnum);
 
                 //test castnum value
                 //System.out.println(castnum);
@@ -37,15 +40,51 @@ public class Dvoracek_Chad_Asgn_2b {
                 //test numberChoice value
                 //System.out.println (numberChoice);
         
-        System.out.println(castnum+" is the number and NumberFun isNumberOdd() returns: "+ numberChoice);
-       
-        }
         
-      /** Deliverable B
-       * 
-       * 
-       */
+        
+        System.out.println("isNumberOdd is "+numberChoice+" for the number "+castnum);
+    
+        }
+        System.out.println ("\r");
+        
+        
+    /** Deliverables B
+     *  Provide a value for the method NumberFun and test
+     *  if the number is odd.
+     */
+        
+        
+        for (castnum=0; castnum<2; castnum++)
+        { 
+            NumberFun numFun2= new NumberFun();
+            boolean numberChoice2 = numFun2.isNumberEven(castnum);
 
-    }
+                //test castnum value
+                //System.out.println(castnum);
+   
+                //test numberChoice value
+                //System.out.println (numberChoice2);
+        
+        System.out.println("isNumberEven is "+numberChoice2+" for the number "+castnum); 
+        }
+        System.out.println("\r");
+        
+    /** Deliverables C
+     *  Provide a value for the method NumberFun hasSameEvenOddStatus and test
+     *  if the number even and if both numbers are either even, odd, or both.
+     * 
+     *  I thought it would be fun to have the last part generate random numbers
+     *  for the input that would allow the testing of the different statements.
+     *  I found the following formula on the internet at:
+     *  http://stackoverflow.com/questions/363681/generating-random-number-in-a-range-with-java
+     */
+       
+        
+        random1=0 + (int)(Math.random() * ((100 - 0) + 1));
+        random2=0 + (int)(Math.random() * ((100 - 0) + 1));
+        NumberFun numFun3 = new NumberFun();
+        numFun3.hasSameEvenOddStatus(random1,random2);
+        
+        
 }
-
+}
