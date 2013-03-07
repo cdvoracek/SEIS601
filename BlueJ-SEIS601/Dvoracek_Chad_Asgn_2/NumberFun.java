56776762a3d26dc5eeb603ca6b4bd2480b87b394
cdvoracek <chad.dvoracek@gmail.com>
2013-03-06 23:14:00 -0600

    ComBJ_asgn_2
    Final

--- BlueJ-SEIS601/Dvoracek_Chad_Asgn_2/NumberFun.java
+++ BlueJ-SEIS601/Dvoracek_Chad_Asgn_2/NumberFun.java
@@ -6,19 +6,44 @@
  * @version (a version number or a date)
  */
 
-public class NumberFun
+public class NumberFun 
 {
 
+    //Assign Variables
+    boolean num1status, num2status;
     boolean isNumberOdd(int num)
-   {num=8;
-    
-    if (num/2 = type.int){ 
-        isNumberOdd=false;	
-    
-    else {isNumberOdd=true;
+    {       
+        if (num%2==0){return false;} 	
+        else {return true;}
     }
-    System.out.println (isNumberOdd);  
 
-}
+    boolean isNumberEven(int num)
+    {   if (num%2==0) {return true;}
+        else {return false;}
+    }
+
+    public void hasSameEvenOddStatus(int num1,int num2)
+    {
+
+        boolean num1status, 
+        num2status;
+
+        String declare1, 
+        declare2, 
+        compareboth;
+
+        if (num1 % 2==0){num1status = true; declare1="even";}
+        else {num1status = false; declare1="odd";}
+
+        if (num2 % 2==0) {num2status = true; declare2="even";}
+        else {num2status = false; declare2="odd";}
+
+        if (num1status == num2status){compareboth = "Both numbers ("+num1+" & "+num2+") are "+declare1;}
+        else {compareboth = "The first number ("+num1+") is "+declare1+" and the second number ("+num2+") is "+declare2;}
+
+        System.out.println (num1+" is "+declare1);
+        System.out.println (num2+" is "+declare2);
+        System.out.println (compareboth);
+    }
 }
 
