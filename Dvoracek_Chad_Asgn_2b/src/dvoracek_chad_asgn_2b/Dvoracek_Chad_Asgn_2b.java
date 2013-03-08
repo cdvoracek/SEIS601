36cb00f5f546e4e2ce5a61a84d9ce04ec8965914
cdvoracek <chad.dvoracek@gmail.com>
2013-03-07 20:16:22 -0600

    comedit1

--- Dvoracek_Chad_Asgn_2b/src/dvoracek_chad_asgn_2b/Dvoracek_Chad_Asgn_2b.java
+++ Dvoracek_Chad_Asgn_2b/src/dvoracek_chad_asgn_2b/Dvoracek_Chad_Asgn_2b.java
@@ -15,76 +15,108 @@ public class Dvoracek_Chad_Asgn_2b {
      */
     public static void main(String[] args) {
 
-    //declare variables
-        
-    int castnum,
+        //declare variables
+
+        int castnum,
         castnum2,
         random1,
-        random2;
-    
-   
+        random2,
+        testnum;
+
        
-    /** Deliverables A
-     *  Provide a value for the method NumberFun and test
-     *  if the number is odd.
-     */
-    
+        /** Deliverables A
+         *  Provide a value for the method NumberFun and test
+         *  if the number is odd.
+         */
+
         for (castnum=0; castnum<2; castnum++)
         { 
             NumberFun numFun = new NumberFun();
             boolean numberChoice = numFun.isNumberOdd(castnum);
 
-                //test castnum value
-                //System.out.println(castnum);
-   
-                //test numberChoice value
-                //System.out.println (numberChoice);
+            //test castnum value
+            //System.out.println(castnum);
+
+            //test numberChoice value
+            //System.out.println (numberChoice);
+
         
-        
-        
-        System.out.println("isNumberOdd is "+numberChoice+" for the number "+castnum);
-    
+            System.out.println("isNumberOdd is "+numberChoice+" for the number "+castnum);
+
         }
         System.out.println ("\r");
-        
-        
-    /** Deliverables B
-     *  Provide a value for the method NumberFun and test
-     *  if the number is odd.
-     */
-        
-        
+
+        /** Deliverables B
+         *  Provide a value for the method NumberFun and test
+         *  if the number is odd.
+         */
+
         for (castnum=0; castnum<2; castnum++)
         { 
             NumberFun numFun2= new NumberFun();
             boolean numberChoice2 = numFun2.isNumberEven(castnum);
 
-                //test castnum value
-                //System.out.println(castnum);
-   
-                //test numberChoice value
-                //System.out.println (numberChoice2);
-        
-        System.out.println("isNumberEven is "+numberChoice2+" for the number "+castnum); 
+            //test castnum value
+            //System.out.println(castnum);
+
+            //test numberChoice value
+            //System.out.println (numberChoice2);
+
+            System.out.println("isNumberEven is "+numberChoice2+" for the number "+castnum); 
         }
         System.out.println("\r");
+
+        /** Deliverables C
+         *  Provide a value for the method NumberFun hasSameEvenOddStatus and test
+         *  if the number even and if both numbers are either even, odd, or both.
+         * 
+         *  I thought it would be fun to have the last part generate random numbers
+         *  for the input that would allow the testing of the different statements.
+         *  I found the following formula on the internet at:
+         *  http://stackoverflow.com/questions/363681/generating-random-number-in-a-range-with-java
+         */
+        random1=0;
+        random2=0;
+
+        for (testnum=1; testnum<8; testnum++)
+        {
+            switch (testnum){
+                case 1:
+                random1=-1;
+                random2=4;
+                break;
+                case 2:
+                random1=0;
+                random2=4;
+                break;    
+                case 3:
+                random1=6;
+                random2=57;
+                break;
+                case 4:
+                random1=0;
+                random2=3;
+                break;
+                case 5:
+                random1=119;
+                random2=-37;
+                break;
+                case 6:
+                random1=-4;
+                random2=-3;
+                break;
+                case 7:
+                random1=-202;
+                random2=0;
+                break;
+            }
+            NumberFun numFun3 = new NumberFun();
+            numFun3.hasSameEvenOddStatus(random1,random2); 
+        }
+
+        //random1=0 + (int)(Math.random() * ((100 - 0) + 1));
+        //random2=0 + (int)(Math.random() * ((100 - 0) + 1));
+
         
-    /** Deliverables C
-     *  Provide a value for the method NumberFun hasSameEvenOddStatus and test
-     *  if the number even and if both numbers are either even, odd, or both.
-     * 
-     *  I thought it would be fun to have the last part generate random numbers
-     *  for the input that would allow the testing of the different statements.
-     *  I found the following formula on the internet at:
-     *  http://stackoverflow.com/questions/363681/generating-random-number-in-a-range-with-java
-     */
-       
-        
-        random1=0 + (int)(Math.random() * ((100 - 0) + 1));
-        random2=0 + (int)(Math.random() * ((100 - 0) + 1));
-        NumberFun numFun3 = new NumberFun();
-        numFun3.hasSameEvenOddStatus(random1,random2);
-        
-        
-}
+    }
 }
--- Dvoracek_Chad_Asgn_2b/src/dvoracek_chad_asgn_2b/NumberFun.java
+++ Dvoracek_Chad_Asgn_2b/src/dvoracek_chad_asgn_2b/NumberFun.java
@@ -10,7 +10,7 @@ package dvoracek_chad_asgn_2b;
  */
 public class NumberFun 
 {
-  
+
     //Assign Variables
     boolean num1status, num2status;
     boolean isNumberOdd(int num)
@@ -18,37 +18,35 @@ public class NumberFun
         if (num%2==0){return false;} 	
         else {return true;}
     }
-    
+
     boolean isNumberEven(int num)
     {   if (num%2==0) {return true;}
         else {return false;}
     }
 
-  
- public void hasSameEvenOddStatus(int num1,int num2)
+    public void hasSameEvenOddStatus(int num1,int num2)
     {
-        
+
         boolean num1status, 
-                num2status;
-        
+        num2status;
+
         String declare1, 
-               declare2, 
-               compareboth;
-        
+        declare2, 
+        compareboth;
+
         if (num1 % 2==0){num1status = true; declare1="even";}
         else {num1status = false; declare1="odd";}
-    
+
         if (num2 % 2==0) {num2status = true; declare2="even";}
         else {num2status = false; declare2="odd";}
-        
-        if (num1status == num2status){compareboth = "Both numbers ("+num1+" & "+num2+") are "+declare1;}
-        else {compareboth = "The first number ("+num1+") is "+declare1+" and the second number ("+num2+") is "+declare2;}
-        
-        
+
+        if (num1status == num2status){compareboth = "hasSameEvenOddStatus=True. Both numbers ("+num1+" & "+num2+") are "+declare1+".";}
+        else {compareboth = "hasSameEvenOddStatus=False. The first number ("+num1+") is "+declare1+" and the second number ("+num2+") is "+declare2+".";}
+
         System.out.println (num1+" is "+declare1);
         System.out.println (num2+" is "+declare2);
         System.out.println (compareboth);
+        System.out.println ("\r");
+        
     }
 }
-
-
